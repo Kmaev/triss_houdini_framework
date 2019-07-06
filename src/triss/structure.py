59ff5fd5e80a413ebc4f48b87186f925a95a9a23
kristina_m <kris.workplace@gmail.com>
2019-07-06 12:21:08 +0200

    added **kwargs to folder_structure to import template data, reorganized stuff

---
+++ src/triss/structure.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import json
+import os
+
+
+def get_template(name):
+    path=os.environ.get("FOLDER_STUCTURE_PATH")
+    with open(path) as read_file:
+        file = json.load(read_file)
+    if name not in file.keys():
+        raise ValueError('Template "{}" doesn\'t exist'.format(name))
+    return file[name]
+
+
+def folder_structure(template, data):
+    template_data = get_template(name=template)
+    try:
+        path = template_data.format(**data)
+    except KeyError as e:
+        raise RuntimeError("Not enough template data, {} missed ".format(e))
+    return path
+
